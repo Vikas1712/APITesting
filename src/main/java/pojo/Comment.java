f451de7b59a2e10edb41342c6e3c4ab2df9b6a61
Yadav, Vikas <Vikas.Yadav@bakerhughes.com>
2021-01-08 16:16:08 +0100

    Create Maven project with cucumber and rest assured dependencies
    project structure and feature file added

---
+++ src/main/java/pojo/Comment.java
@@ -0,0 +1,12 @@
+package pojo;
+
+public class Comment {
+    private final String body;
+    private final Integer postId;
+
+    public Comment(String body, Integer postId) {
+        this.body = body;
+        this.postId = postId;
+    }
+
+}
